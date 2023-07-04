c444cb893a6d11ae2062ff3dcd5faf100e697b13
Patrick0x0 <patrick.bichiarov@hotmail.com>
2023-07-04 20:34:55 +0100

    Verificação utils "extract code"

--- TicketRestAPI/tickets/tests/test_utils.py
+++ TicketRestAPI/tickets/tests/test_utils.py
@@ -2,31 +2,17 @@ from unittest import mock
 from unittest.mock import patch, MagicMock
 from django.test import TestCase
 from django.conf import settings
-from tickets.utils import get_body
-from email.message import EmailMessage
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+from tickets.utils import extract_code_from_subject
 
 
-class GetBodyTest(TestCase):
+class ExtractCodeTest(TestCase):
 
-    def test_get_body_for_multipart_message(self):
-        # Criar uma mensagem de e-mail multipart
-        msg = MIMEMultipart()
-        body_part = MIMEText("This is the main content", 'plain')
-        msg.attach(body_part)
-        attachment_part = MIMEText("This is an attachment", 'plain')
-        msg.attach(attachment_part)
+    def test_extract_code_from_subject_with_brackets(self):
+        subject = "Support request [12345]"
+        code = extract_code_from_subject(subject)
+        self.assertEqual(code, "12345")
 
-        # Testar a função get_body
-        body = get_body(msg)
-        self.assertEqual(body, "This is the main content")
-
-    def test_get_body_for_singlepart_message(self):
-        # Criar uma mensagem de e-mail singlepart
-        msg = EmailMessage()
-        msg.set_content("This is the content")
-
-        # Testar a função get_body
-        body = get_body(msg)
-        self.assertEqual(body, "This is the content\n")
+    def test_extract_code_from_subject_without_brackets(self):
+        subject = "Support request 12345"
+        code = extract_code_from_subject(subject)
+        self.assertIsNone(code)
