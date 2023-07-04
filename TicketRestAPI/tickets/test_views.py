4c96035adece090950fe5e49d721e76cf1dfa9d8
SimaoPTSousa <123485421+SimaoPTSousa@users.noreply.github.com>
2023-07-04 15:11:48 +0100

    added comment factory

--- TicketRestAPI/tickets/test_views.py
+++ TicketRestAPI/tickets/test_views.py
@@ -5,16 +5,43 @@ from rest_framework import status
 import django
 django.setup()
 
-from .factories import TicketFactory, TicketThreadFactory
+from .factories import CommentFactory
+from .serializers import CommentSerializer
+from .models import Comment
 
-
-class TicketAPITests(APITestCase):
+class CommentViewSetTest(APITestCase):
     def setUp(self):
-        self.ticket_thread = TicketThreadFactory()
-        self.ticket = TicketFactory(thread=self.ticket_thread)
-        
-    def test_get_ticket_list(self):
-        url = reverse('ticket-list')  # replace 'ticket-list' with your actual url name
+        pass
+
+    def test_list_comments(self):
+        CommentFactory.create_batch(3)
+
+        url = reverse('comments')
         response = self.client.get(url)
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
+        self.assertEqual(len(response.data), 3)
+
+    def test_create_comment(self):
+        url = reverse('comments')
+
+        data = {
+            'ticket': id,
+            'user': id,
+            'text': 'comments'
+        }
+
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data, CommentSerializer(instance=Comment.objects.last()).data)
+
+    def test_delete_comment(self):
+        comment = CommentFactory()
+
+        url = reverse('delete_comment', kwargs={'pk': comment.pk})
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {'status': 'ok'})
+        self.assertFalse(Comment.objects.filter(pk=comment.pk).exists())
