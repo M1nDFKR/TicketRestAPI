509c468eea115c9ce26061a8c016b2cb8e40a22b
Patrick0x0 <patrick.bichiarov@hotmail.com>
2023-07-05 12:11:39 +0100

    Verificação dos models "Ticket"

--- TicketRestAPI/tickets/tests/test_models.py
+++ TicketRestAPI/tickets/tests/test_models.py
@@ -1,13 +1,27 @@
 from django.test import TestCase
-from tickets.models import TicketThread
+from tickets.models import Ticket, TicketThread
 
 
-class TicketThreadTest(TestCase):
+class TicketTest(TestCase):
     def setUp(self):
         self.ticket_thread = TicketThread.objects.create()
+        self.ticket = Ticket.objects.create(
+            title="Test Ticket",
+            code="Test Code",
+            thread=self.ticket_thread
+        )
 
-    def test_ticket_thread_creation(self):
-        self.assertIsInstance(self.ticket_thread, TicketThread)
+    def test_ticket_creation(self):
+        self.assertIsInstance(self.ticket, Ticket)
 
     def test_default_status(self):
-        self.assertEqual(self.ticket_thread.status, 'A')
+        self.assertEqual(self.ticket.status, 'A')
+
+    def test_ticket_thread(self):
+        self.assertEqual(self.ticket.thread, self.ticket_thread)
+
+    def test_ticket_title(self):
+        self.assertEqual(self.ticket.title, "Test Ticket")
+
+    def test_ticket_code(self):
+        self.assertEqual(self.ticket.code, "Test Code")
