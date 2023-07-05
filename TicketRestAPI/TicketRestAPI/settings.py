9f4c1317554b2f89c2a40fde1ff5655576543f85
jfwebmax <jose.ferreira.webmax@gmail.com>
2023-07-05 15:48:23 +0100

    changed settings to allow for CORS.

--- TicketRestAPI/TicketRestAPI/settings.py
+++ TicketRestAPI/TicketRestAPI/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'tickets',
 ]
@@ -53,10 +54,14 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'TicketRestAPI.urls'
 
