7c4d13745fec8dd57c33fa9821f70cb9938d3e5d
Patrick0x0 <patrick.bichiarov@hotmail.com>
2023-07-05 14:42:09 +0100

    Todas os test utils, models, views.

--- TicketRestAPI/tickets/tests/test_models.py
+++ TicketRestAPI/tickets/tests/test_models.py
@@ -3,6 +3,41 @@ from django.contrib.auth.models import User
 from tickets.models import Comment, Ticket, TicketThread
 
 
+class TicketThreadTest(TestCase):
+    def setUp(self):
+        self.ticket_thread = TicketThread.objects.create()
+
+    def test_ticket_thread_creation(self):
+        self.assertIsInstance(self.ticket_thread, TicketThread)
+
+    def test_default_status(self):
+        self.assertEqual(self.ticket_thread.status, 'A')
+
+
+class TicketTest(TestCase):
+    def setUp(self):
+        self.ticket_thread = TicketThread.objects.create()
+        self.ticket = Ticket.objects.create(
+            title="Test Ticket",
+            code="Test Code",
+            thread=self.ticket_thread
+        )
+
+    def test_ticket_creation(self):
+        self.assertIsInstance(self.ticket, Ticket)
+
+    def test_default_status(self):
+        self.assertEqual(self.ticket.status, 'A')
+
+    def test_ticket_thread(self):
+        self.assertEqual(self.ticket.thread, self.ticket_thread)
+
+    def test_ticket_title(self):
+        self.assertEqual(self.ticket.title, "Test Ticket")
+
+    def test_ticket_code(self):
+        self.assertEqual(self.ticket.code, "Test Code")
+
 class CommentTest(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(
