7b1be9c7d36a7693e1f830ef247a3cb27ae979fa
Patrick0x0 <patrick.bichiarov@hotmail.com>
2023-07-06 08:58:52 +0100

    Ticket e Thread a funcionar mas falta cena"views"

--- TicketRestAPI/tickets/utils.py
+++ TicketRestAPI/tickets/utils.py
@@ -1,85 +1,85 @@
 import imapclient
 import email
 import re
-from django.conf import settings
+from email.header import decode_header
 from .models import Ticket, TicketThread
+
+EMAIL = "prometonaoescam@gmail.com"
+PASSWORD = "wmmcblxzxvcocuyz"
 
 
 def get_emails():
-    # Conecta-se ao servidor de e-mail configurado e obtém os e-mails da caixa de entrada.
-    # Retorna uma lista de dicionários contendo os assuntos e corpos dos e-mails.
-    client = imapclient.IMAPClient(settings.MAIL_SERVER)
+    with imapclient.IMAPClient("imap.gmail.com", ssl=True) as client:
+        client.login(EMAIL, PASSWORD)
+        client.select_folder("INBOX")
 
-    client.login(settings.MAIL_USERNAME, settings.MAIL_PASSWORD)
+        messages = []
+        # Search for all messages from a specific sender (noreply.escoladigital@min-educ.pt)
+        sender = "noreply.escoladigital@min-educ.pt"
+        message_ids = client.search(['FROM', sender])
+        for message_id in message_ids:
+            msg_data = client.fetch([message_id], ["RFC822"])[message_id]
+            msg = email.message_from_bytes(msg_data[b"RFC822"])
+            subject, encoding = decode_header(msg["Subject"])[0]
+            if isinstance(subject, bytes):
+                subject = subject.decode(encoding or "utf-8")
+            body = ""
+            if msg.is_multipart():
+                for part in msg.walk():
+                    if part.get_content_type() == "text/plain":
+                        body = part.get_payload(decode=True)
+                        break
+            else:
+                body = msg.get_payload(decode=True)
 
-    client.select_folder('INBOX')
+            messages.append(
+                # Decodifica o corpo do email para uma string
+                {"subject": subject, "body": body.decode()}
+            )
 
-    messages = client.search([u'FROM', settings.MAIL_USERNAME])
-
-    response = client.fetch(messages, ['BODY[]'])
-
-    emails = []
-    for msgid, data in response.items():
-        raw_email = data[b'BODY[]']
-        email_message = email.message_from_bytes(raw_email)
-        subject = email_message.get('Subject', '')
-        body = get_body(email_message)
-        emails.append({'subject': subject, 'body': body})
-    return emails
-
-
-def get_body(email_message):
-    # Extrai o corpo de um e-mail.
-    # Se o e-mail for multipart, retorna o corpo do primeiro texto encontrado.
-    # Caso contrário, retorna o corpo do próprio e-mail.
-    if email_message.is_multipart():
-        for part in email_message.get_payload():
-            if part.get_content_type() == 'text/plain':
-                return part.get_payload()
-    else:
-        return email_message.get_payload()
+        return messages
 
 
-def extract_code_from_subject(subject):
-    # Extrai o código entre colchetes do assunto do e-mail.
-    match = re.search(r'\[(.*?)\]', subject)
-    return match.group(1) if match else None
-
-
-def create_or_update_ticket(subject, body, code):
-    # Cria ou atualiza um ticket com base no código.
-    # Retorna a instância do ticket criado ou atualizado.
-    thread, _ = TicketThread.objects.get_or_create(thread_code=code)
-    ticket, created = Ticket.objects.get_or_create(code=code,
-                                                   defaults={'title': subject, 'body': body, 'thread': thread})
-    if not created:
-        ticket.title = subject
-        ticket.body = body
-        ticket.thread = thread
-        ticket.save()
-    return ticket
-
-
-def update_ticket_and_thread_status(ticket_instance, subject):
-    # Atualiza o status do ticket e do thread com base no assunto do e-mail.
-    # Se o assunto contiver "fechado" ou "resolvido", o status é definido como "closed".
-    if "fechado" in subject or "resolvido" in subject:
-        ticket_instance.status = "closed"
-        ticket_instance.save()
-        thread = TicketThread.objects.get(tickets=ticket_instance)
-        thread.status = "closed"
-        thread.save()
-
-
-def fetch_and_process_emails():
-    # Obtém os e-mails, cria ou atualiza os tickets correspondentes e atualiza os status.
+def create_ticket_instances():
     emails = get_emails()
     for email_data in emails:
-        title = email_data['subject']
+        subject = email_data['subject']
         body = email_data['body']
-        code = extract_code_from_subject(title)
-        if code is None:
-            print(f"Could not extract code from email with title: {title}")
-            continue
-        ticket_instance = create_or_update_ticket(title, body, code)
-        update_ticket_and_thread_status(ticket_instance, title)
+
+        # Extract the code from the subject using regular expressions
+        code_regex = r"\[(.*?)\]"
+        match = re.search(code_regex, subject)
+        if match:
+            code = match.group(1)
+        else:
+            code = ""
+
+        # Check if a ticket with the same subject already exists
+        existing_ticket = Ticket.objects.filter(title=subject).first()
+
+        if existing_ticket:
+            # If a ticket with the same subject exists, update its details
+            existing_ticket.body = body
+            existing_ticket.save()
+            ticket_instance = existing_ticket
+        else:
+            # Create a new ticket instance
+            ticket_instance = Ticket(
+                title=subject, body=body, code=code)
+
+            if code:
+                ticket_thread, _ = TicketThread.objects.get_or_create(
+                    thread_code=code)
+                ticket_instance.thread = ticket_thread
+
+            ticket_instance.save()
+
+        # Check if the subject contains "fechado/resolvido"
+        if "fechado/resolvido" in subject.lower():
+            ticket_instance.status = 'F'  # Update status to "Fechado"
+            ticket_instance.save()
+            thread = ticket_instance.thread
+            # Atualizar status da thread para "Fechado"
+            thread.status = ticket_instance.status
+            thread.save()
+            thread.tickets.update(status='F')
