64f86a9417f8a64f34cc0759674d321419bceb4b
SimaoPTSousa <123485421+SimaoPTSousa@users.noreply.github.com>
2023-07-07 14:28:39 +0100

    PDF para tickets

--- TicketRestAPI/tickets/admin.py
+++ TicketRestAPI/tickets/admin.py
@@ -69,38 +69,39 @@ class CustomUserAdmin(UserAdmin):
         doc.build(elements)  # Gera o PDF com os elementos
         return response
 
-class CustomTicketAdmin(admin.ModelAdmin):
-    actions = ['download_user_ticket_pdf']
+class CustomTicketThreadAdmin(admin.ModelAdmin):
+    actions = ['download_ticket_thread_pdf']
 
-    def download_user_ticket_pdf(self, request, queryset):
+    def download_ticket_thread_pdf(self, request, queryset):
         # Cria uma resposta HTTP para o arquivo PDF
         response = HttpResponse(content_type='application/pdf')
-        response['Content-Disposition'] = 'attachment; filename="user_tickets.pdf"'
+        response['Content-Disposition'] = 'attachment; filename="user_ticket_thread.pdf"'
 
         # Cria um objeto SimpleDocTemplate para gerar o PDF
         doc = SimpleDocTemplate(response, pagesize=letter)
         elements = []  # Lista para armazenar os elementos do PDF
 
-        for user in queryset:
-            thread_code = user.thread.thread_code  # Use o campo thread_code como o nome da thread
-            ticket_info = f"Tickets for User: {thread_code}"
+        for thread in queryset:
+            thread_code = thread.thread_code  # Use o campo thread_code como o nome da thread
+            ticket_info = f"Ticket thread: {thread_code}"
 
             styles = getSampleStyleSheet()
             title = Paragraph(ticket_info, styles['Title'])
             elements.append(title)  # Adiciona o título ao PDF
 
-            # Recupera os tickets do usuário e cria a tabela de dados
-            tickets = Ticket.objects.filter(thread=user.thread)  # Corrigido para thread=user.thread
-            data = [['ID', 'Código do Ticket', 'Criado em', 'Status']]
+            # Recupera os tickets da thread e cria a tabela de dados
+            tickets = thread.tickets.all()
+            data = [['ID', 'Data', 'Hora', 'Status']]
             for ticket in tickets:
                 ticket_id = ticket.id
-                ticket_code = ticket.code  # Use o campo ticket_code como o código do ticket
-                created_at = ticket.created_at.astimezone(timezone.get_current_timezone()).strftime('%Y-%m-%d %H:%M:%S')
+                date = ticket.date + timedelta(days=1)  # Adicione um timedelta para ajustar a data conforme necessário
+                created_at = date.strftime('%Y-%m-%d')  # Formate a data como string
+                created_time = date.strftime('%H:%M:%S')  # Formate a hora como string
                 status = ticket.status
 
-                data.append([str(ticket_id), ticket_code, created_at, status])  # Converta o ticket_id para uma string
+                data.append([str(ticket_id), created_at, created_time, status])  # Converta o ticket_id para uma string
 
-            table = Table(data, colWidths=[1*inch, 2*inch, 2*inch, 1*inch])  # Ajuste a largura das colunas conforme necessário
+            table = Table(data, colWidths=[1*inch, 1.5*inch, 1*inch, 1*inch])  # Ajuste a largura das colunas conforme necessário
             table_style = TableStyle([
                 ('BACKGROUND', (0, 0), (-1, 0), colors.HexColor('#6495ED')),
                 ('TEXTCOLOR', (0, 0), (-1, 0), colors.white),
@@ -118,7 +119,8 @@ class CustomTicketAdmin(admin.ModelAdmin):
         doc.build(elements)  # Gera o PDF com os elementos
         return response
 
+
 admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
-admin.site.register(Ticket, CustomTicketAdmin)
-admin.site.register([TicketThread, Comment, Registro])
+admin.site.register(TicketThread, CustomTicketThreadAdmin)
+admin.site.register([Ticket, Comment, Registro])
